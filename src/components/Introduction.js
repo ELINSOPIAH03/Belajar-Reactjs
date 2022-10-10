4cc9f9da4b0bb910a4d6cd71ea84fdaebfd14b5f
Elin Sopiah <gitgit367@gmail.com>
2022-10-10 21:27:57 +0700

    first commit

---
+++ src/components/Introduction.js
@@ -0,0 +1,14 @@
+import '../App.css';
+
+function Introduction() {
+    return (
+        <div>
+            <header>
+                <h1>Introduction</h1>
+                <p>Perkenalkan Nama Saya Elin Sopiah</p>
+            </header>
+        </div>
+    );
+}
+
+export default Introduction;
